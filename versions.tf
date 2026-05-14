d276df0401bbdf9c6063e70a3a468a268442c49c
Alister <alister.greg@gmail.com>
2026-05-14 20:56:24 +0100

    add terraform state bucket

--- versions.tf
+++ versions.tf
@@ -7,4 +7,10 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "github-iac-terraform-state"
+    key    = "github-iac/terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
